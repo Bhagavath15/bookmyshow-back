acb9b6efcd0c323637bc2af4de92682935b7601e
bhagavath15 <bhagavath.sbkgmail.com>
2023-03-15 20:44:11 +0530

    book my show backend

---
+++ index.js
@@ -0,0 +1,32 @@
+
+import express from "express"; // "type": "module"
+import { MongoClient } from 'mongodb'
+import * as dotenv from 'dotenv'
+import signinRouter from './router/login.router.js';
+import { auth } from "./middleware/auth.js";
+import cors from "cors";
+
+// import bcrypt   from ' bcrypt'
+dotenv.config()
+const app = express();
+
+const PORT = process.env.PORT;
+
+const MONGO_URL = (process.env.MONGO_URL)
+export const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log('mongo is connected!!');
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/", auth, function (request, response) {
+    response.send("🙋‍♂️, 🌏 🎊✨🤩");
+});
+
+
+
+app.use("/movies", signinRouter);
+
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
